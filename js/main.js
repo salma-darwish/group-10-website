f3c2e4f9c6a0714e9109f2f7aab35125805753b3
salma-darwish <85557606+salma-darwish@users.noreply.github.com>
2021-06-08 13:24:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var getUserName = prompt("Hello! Who are you :)");
+
+console.log(getUserName);
+
+document.getElementById("username").innerText =getUserName;
+
+// tooltip
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
